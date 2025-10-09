75966adae546385276fecafe351bf6d79d4d8d08
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-10-09 23:12:20 +0500

    Task2

---
+++ Tyuiu.KuchukIA.Sprint3.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Security;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.KuchukIA.Sprint3.Task2.V5.Lib
+{
+    public class DataService : ISprint3Task2V5
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sum = 0;
+            do
+            {
+                sum = sum + ((Math.Pow(value, startValue) + 1) * Math.Cos(startValue));
+                startValue++;
+            }
+            while (startValue <= stopValue);
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.KuchukIA.Sprint3.Task2.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Tyuiu.KuchukIA.Sprint3.Task2.V5.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: И.А. Кучук | ПИНб-25-1";
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* Спринт #3                                                                                   *");
+        Console.WriteLine("* Тема: Алгоритмы циклической структуры                                                       *");
+        Console.WriteLine("* Задание #2                                                                                  *");
+        Console.WriteLine("* Вариант #5                                                                                  *");
+        Console.WriteLine("* Выполнил: Кучук Иван Александрович |  ПИНб-25-1                                             *");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                                    *");
+        Console.WriteLine("* аписать программу используя цикл do...while, которая вычисляет сумму ряда по формуле,       *");
+        Console.WriteLine("* при a=0,75                                                                                  *");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                            *");
+        Console.WriteLine("***********************************************************************************************");
+
+
+        double value = 0.75;
+        int startValue = 1;
+        int stopValue = 20;
+
+        Console.WriteLine("Знаение a = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+
+        Console.WriteLine();
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                  *");
+        Console.WriteLine("***********************************************************************************************");
+
+        Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+
+        Console.ReadKey();
+
+    }
+}
